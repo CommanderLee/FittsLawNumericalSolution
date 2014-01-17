b61e52f10e229523f5c945b2461d8569f07a07ba
LiZhen <dao.yi.er.san@gmail.com>
2014-01-17 13:06:21 +0800

    pre-allocate the memory
    pre-allocate the 'stat' matrix to speed up.

--- func.m
+++ func.m
@@ -1,24 +1,27 @@
-function out = func(sigma)
+function out = func(T_)
 % The main function of the project, reflect the Error Rate.
 
-global A W k ER T_
+global A W k ER T sigma
 
 dL = 0.1;
 % Gauss-Legendre Coefficients
 xk = [-0.9324695 0.9324695 -0.6612094 0.6612094 -0.2386192 0.2386192];
 Ak = [ 0.1713245 0.1713245  0.3607616 0.3607616  0.4679139 0.4679139];
 sum = 0.0;
-inf = max(A * 0.2, W * 1.5);
+inf = W;
 
+fprintf('func.m\n');
 for a = (-inf):dL:(-W/2 - dL)
     b = a + dL;
-    fprintf('T_:%f, a=%f, b=%f.\n', T_, a, b);
     p1 = (b - a) / 2;
     p2 = (a + b) / 2;
+    subSum = 0.0;
     for k = 1:6
         L = p1 * xk(k) + p2;
-        sum = sum + Ak(k) * p1 * OuterIntegrand(L, sigma);
+        subSum = subSum + Ak(k) * p1 * OuterIntegrand(L, T_);
     end
+    sum = sum + subSum;
+    fprintf('T_:%f, a=%f, b=%f, subSum=%f.\n', T_, a, b, subSum);
 end
 out = 1 / (sqrt(2 * pi) * sigma) * sum - ER / 4;
 
--- main.m
+++ main.m
@@ -1,12 +1,22 @@
 % The main procedure of the numerical analysis of Fitts' Law.
 Ai = [300];
 Wi = [25];
-ki = [2];
-ERi = [0.04];
+ki = [0.015];
+ERi = [0.4];
+Ti = (0.1:0.1:1);
 
-global A W k ER T_;
-t = (0:0.1:1);
-stat = [];
+[ra ca] = size(Ai);
+[rw cw] = size(Wi);
+[rk ck] = size(ki);
+[re ce] = size(ERi);
+[rt ct] = size(Ti);
+
+N = ca * cw * ck * ce * ct;
+
+global A W k ER T sigma;
+
+stat = zeros(N, 7);
+cnt = 0;
 
 for aa = 1:1
     for ww = 1:1
@@ -19,14 +29,16 @@ for aa = 1:1
                 
                 for tt = 1:10
                     s0 = 5;
-                    T_ = t(tt);
-                    sigma = fsolve(@func, s0, optimset('Display', 'iter'));
-                    T = k * A / sigma;
+                    T = Ti(tt);
+                    sigma = k * A / T;
+                    T_ = fsolve(@func, s0, optimset('Display', 'iter'));
                     MT = T + T_;
-                    stat = [stat; A W k ER T_ MT];
+                    cnt = cnt + 1;
+                    stat(cnt, :) = [A W k ER T T_ MT];
                 end
             end
         end
     end
 end
 plot(stat(:, 5), stat(:, 6));
+stat
