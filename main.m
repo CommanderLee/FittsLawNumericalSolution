8d7a0ae03041db50a1b0f53cae25d3a559eae7a7
Li Zhen (Peter Lee) <dao.yi.er.san@gmail.com>
2014-01-22 12:48:06 +0800

    New ‘main.m' with Trichotomy
    Calculate answers at Trisection points and decrese the range.

--- main.m
+++ main.m
@@ -1,16 +1,21 @@
 % The main procedure of the numerical analysis of Fitts' Law.
 Ai = [300 200 400 500];
-Wi = [25 30];
-ki = [0.015 0.020];
-ERi = [0.04 0.06];
-dt = 0.01;
-Ti = (0.1:dt:3.0);
+Wi = [25 30 35 40];
+ki = [0.015];
+ERi = [0.04];
+Tleft = 0.1;
+Tright = 3;
+Tdepth = 20;
+Tnum = 100;
+
+resultFileName = 'result1.csv';
+% dt = 0.01;
+% Ti = (0.1:dt:3.0);
 
 [ra ca] = size(Ai);
 [rw cw] = size(Wi);
 [rk ck] = size(ki);
 [re ce] = size(ERi);
-[rt ct] = size(Ti);
 
 isplot = 0;
 
@@ -29,49 +34,93 @@ for aa = 1:ca
                 k = ki(kk);
                 ER = ERi(ee);
                 
-                fprintf('[A=%f, W=%f, k=%f, ER<=%f] start.\n', A, W, k, ER);
-                % Calculate for 4 times to get a more precise peak.
-                for ii = 1:4
-                    N = ct;
-                    stat = zeros(N, 7);
-                    cnt = 0;
-                    
-                    for tt = 1:ct
-                        T = Ti(tt);
-                        T_ = 0;
-                        sig = k * A / T;
-                        ER1 = 2 * normcdf(-W/2, 0, sig);
-                        if ER1 <= ER
-                            T_ = 0;
-                        else
-                            ER2 = ER / ER1;
-                            sig_ = fsolve(@getSig_, 10, optimset('Display', 'off'));
-                            %sig_ = fsolve(@getSig_, 10, optimset('Display', 'iter'));
-                            T_ = func(W, k, sig, sig_);
-                        end
-                        MT = T + T_;
-                        cnt = cnt + 1;
-                        stat(cnt, :) = [A W k ER T T_ MT];
+                % The initial bound of T.
+                Ta = Tleft;
+                Tb = Tright;
+                
+                fprintf('[A=%f, W=%f, k=%f, ER<=%f] start.', A, W, k, ER);
+                % Calculate for Tdepth times to get a more precise peak.
+                % Using Trichotomy, calculate answer at T1=(2Ta+Tb)/3,
+                % and T2=(Ta+2Tb)/3
+                for ii = 1:Tdepth
+                    % The first Trisection point.
+                    T1 = (2*Ta + Tb) / 3;
+                    T1_ = 0;
+                    sig = k * A / T1;
+                    ER1 = 2 * normcdf(-W/2, 0, sig);
+                    if ER1 <= ER
+                        T1_ = 0;
+                    else
+                        ER2 = ER / ER1;
+                        sig_ = fsolve(@getSig_, 10, optimset('Display', 'off'));
+                        % sig_ = fsolve(@getSig_, 10, optimset('Display', 'iter'));
+                        T1_ = func(W, k, sig, sig_);
                     end
+                    MT1 = T1 + T1_;
+                    
+                    % The second Trisection point.
+                    T2 = (Ta + 2*Tb) / 3;
+                    T2_ = 0;
+                    sig = k * A / T2;
+                    ER1 = 2 * normcdf(-W/2, 0, sig);
+                    if ER1 <= ER
+                        T2_ = 0;
+                    else
+                        ER2 = ER / ER1;
+                        sig_ = fsolve(@getSig_, 10, optimset('Display', 'off'));
+                        % sig_ = fsolve(@getSig_, 10, optimset('Display', 'iter'));
+                        T2_ = func(W, k, sig, sig_);
+                    end
+                    MT2 = T2 + T2_;
+                    
+                    if MT1 > MT2
+                        Ta = T1;
+                    else
+                        Tb = T2;
+                    end
+                end
+                
+                dt = (Tb - Ta) / Tnum;
+                Ti = (Ta:dt:Tb);
+                [rt ct] = size(Ti);
+                
+                N = ct;
+                stat = zeros(N, 7);
+                cnt = 0;
+                for tt=1:ct
+                    T = Ti(tt);
+                    T_ = 0;
+                    sig = k * A / T;
+                    ER1 = 2 * normcdf(-W/2, 0, sig);
+                    if ER1 <= ER
+                        T_ = 0;
+                    else
+                        ER2 = ER / ER1;
+                        sig_ = fsolve(@getSig_, 10, optimset('Display', 'off'));
+                        % sig_ = fsolve(@getSig_, 10, optimset('Display', 'iter'));
+                        T_ = func(W, k, sig, sig_);
+                    end
+                    MT = T + T_;
+                    
+                    cnt = cnt + 1;
+                    stat(cnt, :) = [A W k ER T T_ MT];
                     
                     if isplot
                         subplot(2, 2, ii);
                         plot(stat(:, 5), stat(:, 7));
                     end
-                    
-                    %stat
-                    [minMT loc] = min(stat(:, 7));
-                    peakT = stat(loc(1), 5);
-                    Ti = (peakT - 10*dt) : (dt/10) : (peakT + 10*dt);
-                    dt = dt / 10;
-                    [rt ct] = size(Ti);
                 end
+                % stat
+                [minMT loc] = min(stat(:, 7));
+                
                 cntR = cntR + 1;
                 results(cntR, :) = stat(loc(1), :);
+                fprintf('...End.\n');
             end
         end
     end
 end
+
 
 for rr=1:resN
     fprintf('The MTmin=%f, with T=%f, T''=%f.\nConstraint: A=%f, W=%f, k=%f, ER<=%f.\n\n',...
@@ -79,4 +128,4 @@ for rr=1:resN
         results(rr, 2), results(rr, 3), results(rr, 4));
 end
 
-csvwrite('result.csv', results);
+csvwrite(resultFileName, results);
