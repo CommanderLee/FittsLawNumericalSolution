5d71f1f5144a27870c813c0fcae60923cd7fca80
Li Zhen (Peter Lee) <dao.yi.er.san@gmail.com>
2014-01-22 13:23:14 +0800

    Evaluate the result with two methods
    MT = a + b * sqrt(A / W)
    MT = a + b * log(A / W + 1)

---
+++ Evaluate.m
@@ -0,0 +1,49 @@
+% Evaluate the results of the Two-Submovement model.
+
+InputFileName = 'result1.csv';
+OutputFileName = 'evaluation1.csv';
+isPlot = 1;
+
+% A W k ER T T_ MT
+mat = csvread(InputFileName);
+
+% Evaluate with MT = a + b*sqrt(A/W)
+X = sqrt(mat(:, 1) ./ mat(:, 2));
+Y = mat(:, 7);
+p = polyfit(X, Y, 1);
+F = polyval(p, X);
+r = corrcoef(X, Y);
+
+A = [p(1, 2)];
+B = [p(1, 1)];
+R = [r(1, 2)];
+
+if isPlot
+    plot(X, F, 'r');
+    hold on;
+    plot(X, Y, 'r.');
+    hold on;
+end
+
+% Evaluate with MT = a + b*log(A/W + 1)
+X = log(mat(:, 1) ./ mat(:, 2) + 1);
+Y = mat(:, 7);
+p = polyfit(X, Y, 1);
+F = polyval(p, X);
+r = corrcoef(X, Y);
+
+A = [A; p(1, 2)];
+B = [B; p(1, 1)];
+R = [R; r(1, 2)];
+
+if isPlot
+    plot(X, F, 'b');
+    hold on;
+    plot(X, Y, 'b.');
+    hold on;
+end
+
+csvwrite(OutputFileName, [A B R]);
+A
+B
+R
