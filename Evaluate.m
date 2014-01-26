de9ef3474692f2ee8b7881d1409a8f25e3a4297e
LiZhen <dao.yi.er.san@gmail.com>
2014-01-26 19:18:15 +0800

    Support multi k and ER now
    The 'Evaluate.m' can process data with multi k and ER.

--- Evaluate.m
+++ Evaluate.m
@@ -1,54 +1,85 @@
 % Evaluate the results of the Two-Submovement model.
 
-InputFileName = 'result7.csv';
-OutputFileName = 'evaluation7.csv';
+InputFileName = 'result8.csv';
+OutputFileName = 'evaluation8.csv';
 isPlot = 1;
 
 % A W k ER ER1 ER2 T T_ MT
 % skip the title
 mat = csvread(InputFileName, 1, 0);
 
-% Evaluate with MT = a + b*sqrt(A/W)
-X = sqrt(mat(:, 1) ./ mat(:, 2));
-Y = mat(:, 9);
-p = polyfit(X, Y, 1);
-F = polyval(p, X);
-r = corrcoef(X, Y);
+% sort by k and ER
+sortMat = sortrows(mat, 3);
+sortMat = sortrows(sortMat, 4);
 
-A = [p(1, 2)];
-B = [p(1, 1)];
-R = [r(1, 2)];
+[sortMatR sortMatC] = size(sortMat);
+currk = sortMat(1, 3);
+currER = sortMat(1, 4);
+st = 1;
+ed = 1;
+cnt = 0;
 
-if isPlot
-    plot(X, F, 'r');
-    hold on;
-    plot(X, Y, 'r.');
-    hold on;
-end
+A = zeros(sortMatR, 2);
+B = zeros(sortMatR, 2);
+R = zeros(sortMatR, 2);
 
-% Evaluate with MT = a + b*log(A/W + 1)
-X = log(mat(:, 1) ./ mat(:, 2) + 1);
-Y = mat(:, 9);
-p = polyfit(X, Y, 1);
-F = polyval(p, X);
-r = corrcoef(X, Y);
-
-A = [A; p(1, 2)];
-B = [B; p(1, 1)];
-R = [R; r(1, 2)];
-
-if isPlot
-    plot(X, F, 'b');
-    hold on;
-    plot(X, Y, 'b.');
-    hold on;
+for rr = 1:sortMatR
+    if sortMat(rr, 3) ~= currk || sortMat(rr, 4) ~= currER || rr == sortMatR
+        % start with new k and ER.
+        if rr == sortMatR
+            ed = rr;
+        else
+            ed = rr - 1;
+            currk = sortMat(rr, 3);
+            currER = sortMat(rr, 4);
+        end
+        cnt = cnt + 1;
+        
+        % Evaluate with MT = a + b*sqrt(A/W)
+        X = sqrt(sortMat(st:ed, 1) ./ sortMat(st:ed, 2));
+        Y = sortMat(st:ed, 9);
+        p = polyfit(X, Y, 1);
+        F = polyval(p, X);
+        r = corrcoef(X, Y);
+        
+        A(cnt, 1) = [p(1, 2)];
+        B(cnt, 1) = [p(1, 1)];
+        R(cnt, 1) = [r(1, 2)];
+        
+        if isPlot
+            plot(X, F, 'r');
+            hold on;
+            plot(X, Y, 'r.');
+            hold on;
+        end
+        
+        % Evaluate with MT = a + b*log(A/W + 1)
+        X = log(sortMat(st:ed, 1) ./ sortMat(st:ed, 2) + 1);
+        Y = sortMat(st:ed, 9);
+        p = polyfit(X, Y, 1);
+        F = polyval(p, X);
+        r = corrcoef(X, Y);
+        
+        A(cnt, 2) = [p(1, 2)];
+        B(cnt, 2) = [p(1, 1)];
+        R(cnt, 2) = [r(1, 2)];
+        
+        if isPlot
+            plot(X, F, 'b');
+            hold on;
+            plot(X, Y, 'b.');
+            hold on;
+        end
+        
+        st = rr;
+    end
 end
 
 fid = fopen(OutputFileName, 'w');
-fprintf(fid, 'A,B,R\n');
+fprintf(fid, 'A1,B1,R1,A2,B2,R2\n');
 fclose(fid);
 
-dlmwrite(OutputFileName, [A B R], '-append');
-A
-B
-R
+dlmwrite(OutputFileName, [A(1:cnt, 1) B(1:cnt, 1) R(1:cnt, 1) A(1:cnt, 2) B(1:cnt, 2) R(1:cnt, 2)], '-append');
+A(1:cnt, :)
+B(1:cnt, :)
+R(1:cnt, :)
